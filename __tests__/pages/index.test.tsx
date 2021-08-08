ae00ae0da32e3fe54c07dbdbdd6d744a3bb6504d
Resi Respati <resir014@gmail.com>
2021-08-08 16:17:28 +0700

    test: fixed tests

--- __tests__/pages/index.test.tsx
+++ __tests__/pages/index.test.tsx
@@ -3,6 +3,8 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import HomePage from "~/pages";
 
+jest.mock("next/router", () => require("next-router-mock"));
+
 describe("HomePage", () => {
   // TODO: add proper tests later
   it("renders without crashing", () => {
