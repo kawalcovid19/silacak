a31cdfe42e47a017e4766da70ba5355a05c87cb0
Resi Respati <resir014@gmail.com>
2021-08-08 13:33:18 +0700

    feat: add WBW tailwind preset (#6)
    * feat: add WBW tailwind preset
    
    This adds the WBW preset for Tailwind CSS.
    
    * chore: add silacak brand color

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
         <h1 className="text-6xl font-bold">
           Welcome to{" "}
-          <a className="text-blue-600" href="https://nextjs.org">
+          <a className="text-brand-500" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,24 @@ module.exports = {
   mode: "jit",
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
+  presets: [require("@kawalcovid19/tailwind-preset-wbw")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        silacak: {
+          50: "#eef5f4",
+          100: "#ccf0f1",
+          200: "#92e8df",
+          300: "#57cfbb",
+          400: "#1fb18f",
+          500: "#159765",
+          600: "#13814e",
+          700: "#13643e",
+          800: "#0f442f",
+          900: "#0a2924",
+        },
+      },
+    },
   },
   variants: {
     extend: {},
