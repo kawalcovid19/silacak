7a4078d12fb1dac1315bf6437a9cfbe919484707
Resi Respati <resir014@gmail.com>
2021-08-08 10:43:43 +0700

    chore: add testing infrastructure

---
+++ __tests__/pages/index.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import HomePage from '~/pages';
+
+import { render, screen } from '@testing-library/react';
+
+describe('HomePage', () => {
+  // TODO: add proper tests later
+  it('renders without crashing', () => {
+    render(<HomePage />);
+
+    expect(screen.getByText(/Welcome to/i)).toBeVisible();
+  });
+});
