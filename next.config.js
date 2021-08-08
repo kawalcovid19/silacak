2ef94c89bd4a02c8cc9b2520a9d66dfc0a7eadf8
Resi Respati <resir014@gmail.com>
2021-08-08 17:48:29 +0700

    perf: next.js optimizations

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import("next/dist/next-server/server/config-shared").NextConfig} */
 module.exports = {
+  // https://github.com/vercel/next.js/blob/v11.0.1/packages/next/next-server/server/config-shared.ts#L42-L65
+  experimental: {
+    optimizeCss: true,
+    optimizeImages: true,
+    workerThreads: true,
+  },
+
+  // https://nextjs.org/docs/advanced-features/customizing-postcss-config
+  future: {
+    strictPostcssConfiguration: true,
+  },
+
+  // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
   reactStrictMode: true,
 };
