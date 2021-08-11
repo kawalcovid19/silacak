296ea49e8d9b26ae149cf7865aa884cbe86e2746
asaadam <graha.asa@gmail.com>
2021-08-11 18:45:00 +0700

    feat: finish layout form

--- pages/registration-status.tsx
+++ pages/registration-status.tsx
@@ -1,4 +1,5 @@
-import { LayoutRoot, LoginForm } from "~/components/layout/home";
+import { CheckAccountForm } from "~/components/layout/checkAccount/check-account-form";
+import { LayoutRoot } from "~/components/layout/home";
 import { HomePageContent } from "~/components/layout/home/home-content";
 import { Container } from "~/components/ui/container";
 
@@ -7,13 +8,13 @@ export default function RegistrationStatus() {
     <LayoutRoot>
       <HomePageContent className="bg-gray-100">
         <Container className="w-full max-w-prose mx-auto">
-          <div className="bg-white shadow-md rounded-lg">
+          <div className="bg-white shadow-md rounded-lg flex-1">
             <div className="w-full max-w-xl mx-auto px-4 py-6 space-y-4">
               <h1 className="text-xl text-silacak-600 text-center">
                 CEK STATUS PENDAFTARAN TRACER
               </h1>
               <div className="space-y-4">
-                <LoginForm />
+                <CheckAccountForm />
               </div>
             </div>
           </div>
