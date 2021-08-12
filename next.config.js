30cad0aa159c6fca0dd8b1a5c4f7a4c2f1c8ac5f
Resi Respati <resir014@gmail.com>
2021-08-12 19:45:22 +0700

    fix: redirect `/dashboard` to `/dashboard/tracing`

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
+/** @type {import("next/dist/lib/config-shared").Header['headers']} */
+const securityHeaders = [
+  {
+    key: "Referrer-Policy",
+    value: "same-origin",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "DENY",
+  },
+  {
+    key: "X-Content-Type-Options",
+    value: "nosniff",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+];
+
 /** @type {import("next/dist/next-server/server/config-shared").NextConfig} */
 module.exports = {
   // https://github.com/vercel/next.js/blob/v11.0.1/packages/next/next-server/server/config-shared.ts#L42-L65
@@ -12,8 +32,29 @@ module.exports = {
     strictPostcssConfiguration: true,
   },
 
+  // This config won't be loaded until Netlify supports the `headers` option on `next.config.js`.
+  // For now, when you make changes here, also make the necessary changes on `netlify.toml`.
+  // https://github.com/netlify/netlify-plugin-nextjs/issues/150
+  headers: async () => {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+
   // https://nextjs.org/docs/api-reference/next.config.js/react-strict-mode
   reactStrictMode: true,
+
+  async redirects() {
+    return [
+      {
+        source: "/dashboard",
+        destination: "/dashboard/tracing",
+      },
+    ];
+  },
 
   // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
   webpack(config, { dev, isServer }) {
