424a928971484ea2a44c99c32e52843e3041d7d8
Resi Respati <resir014@gmail.com>
2021-08-13 23:38:26 +0700

    feat: add puskesmas + close contact count

--- components/tracing/confirmed-cases-card.tsx
+++ components/tracing/confirmed-cases-card.tsx
@@ -4,11 +4,19 @@ import { OutlineButton, PrimaryButton } from "../ui/button";
 export function ConfirmedCasesCard() {
   return (
     <div className="md:flex md:items-center px-4 py-4 sm:px-6">
-      <div className="space-y-2 md:flex-1">
-        <p className="text-sm font-semibold text-silacak-500">Muhammad Nuh</p>
-        <p className="text-sm text-gray-500">
-          No. HP: <strong className="font-bold text-gray-900">+62876085357</strong>
-        </p>
+      <div className="flex-1 md:grid md:grid-cols-2 md:gap-4">
+        <div>
+          <p className="text-sm font-semibold text-silacak-500">Muhammad Nuh</p>
+          <p className="text-sm text-gray-900 font-semibold">PKM. KEL. PETOJO UTARA</p>
+        </div>
+        <div>
+          <p className="text-sm text-gray-500">
+            No. HP: <strong className="font-bold text-gray-900">+62876085357</strong>
+          </p>
+          <p className="text-sm text-gray-500">
+            Jumlah Kontak Erat: <strong className="font-bold text-gray-900">0</strong>
+          </p>
+        </div>
       </div>
       <div className="flex flex-none items-start mt-4 lg:mt-0 lg:ml-4 space-x-4">
         <PrimaryButton icon={PlusIcon}>Kontak Erat</PrimaryButton>
