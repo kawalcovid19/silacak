f2fb64df4bd6c33a1bbca622b23aaf1fcd2ac087
Resi Respati <resir014@gmail.com>
2021-08-21 14:58:29 +0700

    style: minor margin tweaks

--- pages/dashboard/tracing/[caseId]/index.tsx
+++ pages/dashboard/tracing/[caseId]/index.tsx
@@ -38,15 +38,17 @@ export default function TracingCaseDetail() {
       </DashboardPageHeader>
       <DashboardPageContent>
         <div className="space-y-4">
-          <SectionCard className="space-y-4">
-            <SectionCardHeader title="Informasi Utama" />
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
-              <CaseInformationItem label="NIK" value="3401230000000001" />
-              <CaseInformationItem label="Nama Pasien" value="Muhammad Nuh" />
-              <CaseInformationItem label="Tanggal Lahir" value="13 Juni 1995 (26 thn 1 bln)" />
-              <CaseInformationItem label="Jenis Kelamin" value="Laki-laki" />
-              <CaseInformationItem label="No Telp/HP" value="085847475454" />
-              <CaseInformationItem label="Alamat" value="Jl HR Rasuna Said Kav C17" />
+          <SectionCard className="space-y-6">
+            <div className="space-y-4">
+              <SectionCardHeader title="Informasi Utama" />
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-2">
+                <CaseInformationItem label="NIK" value="3401230000000001" />
+                <CaseInformationItem label="Nama Pasien" value="Muhammad Nuh" />
+                <CaseInformationItem label="Tanggal Lahir" value="13 Juni 1995 (26 thn 1 bln)" />
+                <CaseInformationItem label="Jenis Kelamin" value="Laki-laki" />
+                <CaseInformationItem label="No Telp/HP" value="085847475454" />
+                <CaseInformationItem label="Alamat" value="Jl HR Rasuna Said Kav C17" />
+              </div>
             </div>
             <hr />
             <div className="space-y-1">
