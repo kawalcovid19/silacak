413249d5830c7d0f443cd77b793c373f43bf4a0f
Bashir Arrohman <tyangjawi03@gmail.com>
2021-08-23 09:42:05 +0700

    fix: comment log, cause unit test error

--- pages/dashboard/tracing/[caseId]/[contactId]/index.tsx
+++ pages/dashboard/tracing/[caseId]/[contactId]/index.tsx
@@ -10,8 +10,8 @@ import {
 export default function TracingCaseContactDetail() {
   const router = useRouter();
 
-  console.log("caseId", router.query.caseId);
-  console.log("contactId", router.query.contactId);
+  // console.log("caseId", router.query.caseId);
+  // console.log("contactId", router.query.contactId);
 
   return (
     <DashboardPage>
